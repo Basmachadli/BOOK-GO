80d01f0d59d746758d2c29f1c7dda5df3b007ad1
walaedinesekoub <walaedine.sekoubterminal@gmail.com>
2026-01-07 13:39:32 +0100

    maj

---
+++ BookAndGo/src/main/java/test.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class test {
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:oracle:thin:@etu-ora2.ut-capitole.fr:1550:siip";
+        String user = "BOOKANDGO_G7";
+        String password = "root";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        System.out.println("Connexion Oracle OK !");
+        conn.close();
+    }
+}
