464096bd4a320c4377d49090ff06750774d3cacb
walaedinesekoub <walaedine.sekoubterminal@gmail.com>
2026-01-07 12:34:28 +0100

    StartProject
    Début du projets

---
+++ BookAndGo/src/main/java/fr/miage/bookandgo/BookAndGoApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.bookandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAndGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAndGoApplication.class, args);
+    }
+
+}
---
+++ BookAndGo/src/test/java/fr/miage/bookandgo/BookAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.bookandgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
